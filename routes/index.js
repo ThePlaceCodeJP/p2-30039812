37f7022ce0d6b8136c23c2bd0465ed71e219c7f7
ThePlaceCodeJP <valero.joze@yahoo.com>
2022-07-08 22:19:20 -0400

    task5

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const sqlite3=require('sqlite3').verbose();
 const http=require('http');
 const path = require('path');
 const geoip = require('geoip-lite');
 const nodemailer = require('nodemailer');
-const fetch = require('node-fetch');
-const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const PassportLocal = require('passport-local').Strategy;
-require('../setup/passports-setup.js')
+const fetch = require('node-fetch'); 
+require('dotenv').config({path:'../.env'})
 
 const { I18n } = require('i18n')
 const i18n = new I18n({
@@ -19,141 +15,51 @@ const i18n = new I18n({
   defaultLocale: 'es',
 });
 
-require('dotenv').config();
+const db=path.join(__dirname,"database","basededatos.db");
 
-router.use(express.urlencoded({extended: true}));
-router.use(cookieParser(process.env.SECRET));
-router.use(session({
-	secret: process.env.SECRET,
-	resave: true,
-	saveUninitialized: true
-}))
-
-router.use(passport.initialize());
-router.use(passport.session());
-
-passport.use( new PassportLocal(function(username, password, done){
-
-	if(username === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD)
-		return done(null,{id: 1, name: "Admin"});
-
-	done(null, false)
-}))
-
-passport.serializeUser(function(user, done){
-	done(null, user.id)
-})
-
-passport.deserializeUser(function(user, done){
-	done(null,{id: 1, name: "Admin"});
-})
-
-
-
-  
-const basededatos=path.join(__dirname,"basededatos","basededatos.db");
-const bd=new sqlite3.Database(basededatos, err =>{ 
+//conexion con la base de datos 
+const db_run = new sqlite3.Database(db, err =>{ 
 if (err){
-	return console.error(err.message);
+  return console.error(err.message);
 }else{
-	console.log("...");
+  console.log("Base de datos conectada");
 }
 })
 
+//creacion de la base de datos 
+const crear="CREATE TABLE IF NOT EXISTS contactos(nombre VARCHAR(16),email VARCHAR(16), comentario TEXT,fecha DATATIME, ipdire VARCHAR(15), hora VARCHAR(20), geoloca VARCHAR(3));";
 
-const create="CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip TEXT, pais VARCHAR(15));";
-
-bd.run(create,err=>{
-	if (err){
-	return console.error(err.message);
-}else{
-	console.log("...");
-}
-})
-
-router.get('/login',(req,res)=>{
-	res.render('login.ejs')
-});
-
-router.post('/login', passport.authenticate('local',{
-	successRedirect: "/contactos",
-	failureRedirect: "/login"
-}));
-
-router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-
-router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/login'}), function(req, res){
-	res.redirect('/contactos');
-})
-
-router.get('/contactos',(req, res, next)=>{
-	if(req.isAuthenticated()) return next();
-
-	res.redirect("/login")
-},(req,res)=>{
-	const sql="SELECT * FROM contactos;";
-	bd.all(sql, [],(err, rows)=>{
-			if (err){
-				return console.error(err.message);
-			}else{
-			res.render("contactos.ejs",{tarea:rows});
-			}
-	})
-})
-
-
-
-const db=path.join(__dirname,"basedb","base.db");
-const db_run=new sqlite3.Database(db, err =>{ 
-if (err){
-	return console.error(err.message);
-}else{
-	console.log("DB active");
-}
-})
-
-const crear="CREATE TABLE IF NOT EXISTS contacts(email VARCHAR(16),nombre VARCHAR(16), comentario TEXT,fecha DATATIME,ip VARCHAR(15));";
-
-
-
+//anexdo de tabla 
 db_run.run(crear,err=>{
-	if (err){
-	return console.error(err.message);
-}else{
-	console.log("Tb active");
-}
+  if (err){
+  return console.error(err.message);
+  }else{
+  console.log("Tabla anexada");
+  }
 })
-router.get('/',(req,res)=>{
-	res.render('index.ejs',{ct:{}})
-});
-
-db_run.run(crear,err=>{
-	if (err){
-	return console.error(err.message);
-}else{
-	console.log("Tb active");
-}
-})
-
-
 
 router.get('/contactos',(req,res)=>{
-	const sql="SELECT * FROM contacts;";
-	db_run.all(sql, [],(err, rows)=>{
-			if (err){
-				return console.error(err.message);
-			}else{
-			res.render("contactos.ejs",{ct:rows});
-			}
-	})
+  let sql="SELECT * FROM contactos;";
+  db_run.all(sql, [],(err, rows)=>{
+      if (err){
+        return console.error(err.message);
+      }else{
+      res.render("contactos.ejs",{modelo:rows});
+      }
+  })
 })
 
-
+//uso de la api de geolocalizacion 
+// router.get('/',(req, res)=>{
+//   var ip = req.ip
+//   var geo = geoip.lookup(ip);
+//   console.log(geo);
+// });
 
 //ip y la feha, junto con la importacion de los datos a la base 
 router.post('/',(req,res)=>{
     const responseKey = req.body["g-recaptcha-response"];
-    const secretKey = process.env.KEY_SECRET;
+    const secretKey = "6LfRjYcgAAAAALL8ssrUU4Dgm4BwbprYXqOu5lhf";
     const url = 
       `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${responseKey}`;
     fetch(url, {
@@ -177,25 +83,27 @@ router.post('/',(req,res)=>{
           let nombre = req.body.nombre;
           let comentario = req.body.comentario;
 
-          const sql="INSERT INTO contacts(nombre, email, comentario, fecha, ipdire, hora, geoloca) VALUES (?,?,?,?,?,?,?)";
-          const nuevos_mensajes=[nombre,email,comentario,fecha,ip,hora,geoloca];
+          let sql="INSERT INTO contacts(nombre, email, comentario, fecha, ipdire, hora, geoloca) VALUES (?,?,?,?,?,?,?)";
+          let nuevos_mensajes=[nombre,email,comentario,fecha,ip,hora,geoloca];
 
           db_run.run(sql, nuevos_mensajes, err =>{
             if (err){
               return console.error(err.message);
-            } else{
-                res.redirect("/");
+            }else{
+                setTimeout(function(){
+                   res.redirect("/"); 
+                }, 1800);
             }
-          })
-
-		  const transporter = nodemailer.createTransport({
-			host: 'smtp.ethereal.email',
-			port: 587,
-			auth: {
-				user: 'jailyn.hane60@ethereal.email',
-				pass: 'BV9eRS95MXb2CcV5DZ'
-			}
-		});
+            })
+             const transporter = nodemailer.createTransport({
+              host: "smtp-mail.outlook.com",
+              secureConnection: false,
+              port: 587, 
+              auth: {
+                user: "juliocnc19@outlook.com",
+                pass: "jc19012003"
+            }
+            });
           const contenidoHTML = `
             <p>Programacion P2</p>
             <h3>Información del Cliente/Contacto:</h3>
@@ -204,7 +112,7 @@ router.post('/',(req,res)=>{
               <li>Email: ${email}</li>
               <li>Comentario: ${comentario}</li>
               <li>Fecha: ${fecha}</li>
-            <li>Hora: ${hora}</li>
+            <li>Hora: ${hora}
             <li>IP: ${ipdire}</li>
             <li>Pais: ${geoloca}</li>
             </ul>`;
@@ -222,7 +130,8 @@ router.post('/',(req,res)=>{
             else
               console.log(info);
             })
-        }else{
+          
+          }else{
   
                 setTimeout(function(){
               res.redirect("/");        
@@ -232,12 +141,6 @@ router.post('/',(req,res)=>{
                 return res.json({ error });
             });
            
-});
-
-router.get('/',(req,res) => {
-  res.render('index.ejs',{modelo:{},
-  KEY_RECAP:process.env.KEY_RECAP
-})
 });
 
 let translate = false;
@@ -263,4 +166,3 @@ router.get('/translate',(req,res,next)=>{
 });
 
 module.exports = router;
-
